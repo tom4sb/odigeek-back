61f7ba3ed07023411e3e48906b6b63e2a68a5b85
tom4sb <amtomas@proton.me>
2024-11-21 14:56:14 +0100

    Add generated Spring Boot application

---
+++ src/main/java/com/tom4sb/odigeek/OdigeekBackApplication.java
@@ -0,0 +1,13 @@
+package com.tom4sb.odigeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdigeekBackApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(OdigeekBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/tom4sb/odigeek/OdigeekBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom4sb.odigeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdigeekBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
